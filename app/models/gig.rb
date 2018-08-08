311f10a9b36cd894905e6bf523f2f4211b9d1e61
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-08 19:29:02 -0300

    add tags to gigs, dont forget to migrate

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,4 +1,8 @@
 class Gig < ApplicationRecord
   belongs_to :bar
   has_many :applies
+  has_many :bands, through: :applies
+  has_many :gig_tags, dependent: :destroy
+  has_many :tags, through: :gig_tags
+  accepts_nested_attributes_for :gig_tags
 end
