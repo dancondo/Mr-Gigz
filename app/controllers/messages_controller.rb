b059a43aa222ec533ee654a2c2cfae5e897b5726
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-10 01:05:06 -0300

    working action cable but need to implement chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,9 +44,6 @@ class MessagesController < ApplicationController
       @message.bar = Bar.find(@element_id)
     end
     if @message.save
-      # ActionCable.server.broadcast("conversation", {
-      #   message_partial: render(partial: "messages/active_show", locals: { m: @message })
-      # })
       respond_to do |format|
         format.html { redirect_to conversation_url(element_id: @element_id) }
         format.js
