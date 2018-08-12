f90e32e5105beb97d18d80868e11cb6bdb0204dc
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-12 00:33:25 -0300

    g maps implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :bands_home]
 
   def home
-    @bars = Bar.where.not(gigs: nil, latitude: nil, longitude: nil)
   end
 
-  def bands_home
+  def band_home
+    @bars = Bar.where.not(gigs: nil, latitude: nil, longitude: nil)
+    @markers = @bars.map do |bar|
+      {
+        lat: bar.latitude,
+        lng: bar.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/bars/map_box", locals: { bar: bar }) }
+      }
+    end
   end
 end
