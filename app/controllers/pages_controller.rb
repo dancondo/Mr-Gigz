6967d511fbe25279f49409c2a0ca7b5abfbc88df
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-11 23:08:39 -0300

    Bars are now geocoded

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :bands_home]
 
   def home
+    @bars = Bar.where.not(gigs: nil, latitude: nil, longitude: nil)
   end
 
   def bands_home
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,7 +1,17 @@
 class Bar < ApplicationRecord
+  #uploaders
   mount_uploader :photo, PhotoUploader
+
+  #relations
   belongs_to :user
   has_many :gigs
   has_many :messages
+
+  #validations
   validates :user, presence: true
+  validates :address, presence: true
+
+  #geocoding
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
